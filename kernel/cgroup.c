d875324bcb2aff93e1fac9a3512151b6528f5bc3
Emanuel <emanuelghub@gmail.com>
2020-10-08 22:02:02 +0400

    update cgroups boosting

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2948,8 +2948,8 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	if (!ret && !threadgroup &&
 	    !strcmp(of->kn->parent->name, "top-app") &&
 	    task_is_zygote(tsk->parent))
-		cpu_input_boost_kick_max(500);
-
+		cpu_input_boost_kick_max(1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
 	put_task_struct(tsk);
 	goto out_unlock_threadgroup;
 
